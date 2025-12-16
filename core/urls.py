e3250b88b60304ab38e3ea04e1a93f68f3a38af5
Aslon <arslon@gmail.com>
2025-12-16 20:49:58 +0500

    Commit

--- core/urls.py
+++ core/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     # Optional UI:
-    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
