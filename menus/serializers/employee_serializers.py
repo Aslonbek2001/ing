f877721e41687de4af58e5322538eb301ce47ea8
Aslon <arslon@gmail.com>
2025-12-20 13:09:25 +0500

    Commir

--- menus/serializers/employee_serializers.py
+++ menus/serializers/employee_serializers.py
@@ -32,30 +32,11 @@ class EmployeeDetailSerializer(serializers.ModelSerializer):
                     "phone", "email", "image"
                 ]
 
-    def to_internal_value(self, data):
-        data = data.copy()
-        pages = data.get("pages")
-        if hasattr(data, "getlist"):
-            pages = data.getlist("pages") or pages
-        if isinstance(pages, str):
-            data["pages"] = [p.strip() for p in pages.split(",") if p.strip()]
-        elif isinstance(pages, list):
-            normalized = []
-            for item in pages:
-                if isinstance(item, str) and "," in item:
-                    normalized.extend([p.strip() for p in item.split(",") if p.strip()])
-                    continue
-                if isinstance(item, dict) and "id" in item:
-                    normalized.append(item["id"])
-                elif isinstance(item, (list, tuple)):
-                    normalized.extend(item)
-                else:
-                    normalized.append(item)
-            data["pages"] = normalized
-        return super().to_internal_value(data)
-
     def create(self, validated_data):
         pages = validated_data.pop("pages", [])
+        print("##########################")
+        print(pages)
+        print("##########################")
         employee = Employee.objects.create(**validated_data)
         if pages:
             employee.pages.set(pages)
