0aa3f051cfe46e9ba34795ecc5997c8824d85f67
Aslon <arslon@gmail.com>
2025-12-26 11:58:14 +0500

    Commit

--- menus/models.py
+++ menus/models.py
@@ -51,6 +51,9 @@ class Page(models.Model):
     status = models.BooleanField(default=True, db_index=True,)
     description = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return self.title
+
     def save(self, *args, **kwargs):
         if not self.slug:
             if not self.menu:
