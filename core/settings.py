2e63c3090665c9c1adffa3a693a11d8733647414
Aslon <arslon@gmail.com>
2026-01-19 16:59:51 +0500

    Commi

--- core/settings.py
+++ core/settings.py
@@ -21,6 +21,8 @@ ALLOWED_HOSTS = [
     "institute.pythonanywhere.com",
     "127.0.0.1",                    
     "localhost",
+    "api.ing.uz",
+    "172.16.29.126",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
@@ -129,12 +131,24 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ingrnigm',
+        'USER': 'your_project_db',
+        'PASSWORD': 'Qasdz123!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
